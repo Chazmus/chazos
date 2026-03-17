6097c7c576bfcd4a7d106f28176df15dff5ca5c3
Chaz Bailey <charles.s.bailey@gmail.com>
2026-03-17 21:10:58 +0000

    fix(iso): Remove usr/local/bin from profiledef.sh permissions to fix path error

--- chazos_profile/profiledef.sh
+++ chazos_profile/profiledef.sh
@@ -20,8 +20,6 @@ file_permissions=(
   ["/root/.zshrc"]="0:0:600"
   ["/root"]="0:0:750"
   ["/root/.gnupg"]="0:0:700"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/etc/hypr"]="0:0:755"
   ["/etc/hypr/99-chazos-iso.conf"]="0:0:644"
 )
